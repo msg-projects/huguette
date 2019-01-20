a7cd5b32a0770bbe1ca65aa022ce0308a3b37f74
Miguel Sanchez Gonzalez <miguelsanychez@gmail.com>
2019-01-20 22:24:13 +0100

    postcss and SASS configuration

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: {
+        'autoprefixer': {},
+        'postcss-import': {},
+        'postcss-preset-env': {
+            browsers: 'last 2 versions'
+        }
+    }
+};
